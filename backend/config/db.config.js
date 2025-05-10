8bd8a0ac27be1ec2b14a4a866d4dba2721af74d6
Gayan Hewage <gayan.c@outlook.com>
2025-05-10 20:15:08 +1200

    fix: update database configuration with new connection details

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,8 +1,21 @@
+// module.exports = {
+//     HOST: process.env.DB_HOST || '35.213.224.151',
+//     USER: process.env.DB_USER || 'uhex928pteytg',
+//     PASSWORD: process.env.DB_PASSWORD || 'bei0w7c1yvnz',
+//     DB: process.env.DB_NAME || 'dbahuwojk8viis',
+//     dialect: 'postgres',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
 module.exports = {
-    HOST: process.env.DB_HOST || '35.213.224.151',
-    USER: process.env.DB_USER || 'uhex928pteytg',
-    PASSWORD: process.env.DB_PASSWORD || 'bei0w7c1yvnz',
-    DB: process.env.DB_NAME || 'dbahuwojk8viis',
+    HOST: process.env.DB_HOST || '35.213.230.29',
+    USER: process.env.DB_USER || 'ujdtsiltjofcv',
+    PASSWORD: process.env.DB_PASSWORD || 'rofhasalxk5g',
+    DB: process.env.DB_NAME || 'dbudxdgwsnfcfc',
     dialect: 'postgres',
     pool: {
         max: 5,
