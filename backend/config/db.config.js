9e41161e5e1aa4a9d62e1d10ec6406eb052a8271
Gayan Hewage <gayan.c@outlook.com>
2025-07-18 06:13:25 +1200

    feat(db.config): update database configuration for staging environment

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,22 +1,9 @@
 // -- Staging --
-// module.exports = {
-//     HOST: process.env.DB_HOST || '35.213.224.151',
-//     USER: process.env.DB_USER || 'uhex928pteytg',
-//     PASSWORD: process.env.DB_PASSWORD || 'bei0w7c1yvnz',
-//     DB: process.env.DB_NAME || 'dbahuwojk8viis',
-//     dialect: 'postgres',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
 module.exports = {
-    HOST: process.env.DB_HOST || '35.213.230.29',
-    USER: process.env.DB_USER || 'ujdtsiltjofcv',
-    PASSWORD: process.env.DB_PASSWORD || 'rofhasalxk5g',
-    DB: process.env.DB_NAME || 'dbudxdgwsnfcfc',
+    HOST: process.env.DB_HOST || '35.213.224.151',
+    USER: process.env.DB_USER || 'uhex928pteytg',
+    PASSWORD: process.env.DB_PASSWORD || 'bei0w7c1yvnz',
+    DB: process.env.DB_NAME || 'dbahuwojk8viis',
     dialect: 'postgres',
     pool: {
         max: 5,
@@ -25,4 +12,17 @@ module.exports = {
         idle: 10000
     }
 };
+// module.exports = {
+//     HOST: process.env.DB_HOST || '35.213.230.29',
+//     USER: process.env.DB_USER || 'ujdtsiltjofcv',
+//     PASSWORD: process.env.DB_PASSWORD || 'rofhasalxk5g',
+//     DB: process.env.DB_NAME || 'dbudxdgwsnfcfc',
+//     dialect: 'postgres',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
 
